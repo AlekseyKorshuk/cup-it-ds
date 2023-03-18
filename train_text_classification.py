71137edcd62b36e15bd9914a884d8d96aaf64b12
Aleksey Korshuk <ale-kor02@mail.ru>
2023-03-18 11:58:32 -0700

    updates

--- train_text_classification.py
+++ train_text_classification.py
@@ -1,3 +1,4 @@
+import scipy
 from datasets import load_dataset
 
 imdb = load_dataset("AlekseyKorshuk/cup-it-ds-classification")
@@ -20,14 +21,16 @@ data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 import evaluate
 
 accuracy = evaluate.load("accuracy")
+nDCG_metric = evaluate.load('JP-SystemsX/nDCG')
 
 import numpy as np
 
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return accuracy.compute(predictions=predictions, references=labels)
+    results = nDCG_metric.compute(references=labels, predictions=scipy.special.softmax(predictions, axis=1))
+    acc = accuracy.compute(predictions=np.argmax(predictions, axis=1), references=labels)
+    return results.update(acc)
 
 
 id2label = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4"}
