a2154c06213abd242878b2ae389393ac050c108b
Aleksey Korshuk <ale-kor02@mail.ru>
2023-03-18 10:35:10 -0700

    updates

---
+++ train_text_classification.py
@@ -0,0 +1,63 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+from transformers import DataCollatorWithPadding
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+import numpy as np
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+
+id2label = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4"}
+label2id = {"0": 0, "1": 1, "2": 2, "3": 3, "4": 4}
+
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased", num_labels=5, id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    output_dir="/tmp/bert",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+trainer.push_to_hub()
