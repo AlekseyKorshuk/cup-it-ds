0c1a3578c51ab7cb665c5b9109626a590e7cd424
Aleksey Korshuk <ale-kor02@mail.ru>
2023-03-18 00:11:56 -0700

    updates

--- train_reward_model.py
+++ train_reward_model.py
@@ -184,7 +184,7 @@ if __name__ == "__main__":
         evaluation_strategy="steps",
         per_device_train_batch_size=8,
         per_device_eval_batch_size=8,
-        eval_steps=1000,
+        eval_steps=100,
         save_steps=1,
         warmup_steps=100,
         logging_dir="./logs",
