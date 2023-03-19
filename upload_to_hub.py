68d3f98a023076bc2c4e6aab6c05d87a4b2bac2b
Aleksey Korshuk <ale-kor02@mail.ru>
2023-03-19 16:11:25 -0700

    updates

--- upload_to_hub.py
+++ upload_to_hub.py
@@ -5,7 +5,7 @@ repo_id = "AlekseyKorshuk/cup-it-ds-reward-model-with-context"
 api = HfApi()
 api.create_repo(repo_id=repo_id, exist_ok=True)
 api.upload_file(
-    path_or_fileobj="./rm_checkpoint//with-context/checkpoint-5231/pytorch_model.bin",
+    path_or_fileobj="./no-context/with-context/checkpoint-5231/pytorch_model.bin",
     path_in_repo="pytorch_model.bin",
     repo_id=repo_id,
 )
