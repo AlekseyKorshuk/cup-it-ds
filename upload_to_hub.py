20c49039261d1d6922be9876d48e20a5b2c70410
Aleksey Korshuk <ale-kor02@mail.ru>
2023-03-19 16:08:59 -0700

    updates

--- upload_to_hub.py
+++ upload_to_hub.py
@@ -1,11 +1,11 @@
 from huggingface_hub import HfApi
 
-repo_id = "AlekseyKorshuk/cup-it-ds-reward-model-no-context"
+repo_id = "AlekseyKorshuk/cup-it-ds-reward-model-with-context"
 
 api = HfApi()
 api.create_repo(repo_id=repo_id, exist_ok=True)
 api.upload_file(
-    path_or_fileobj="./no-context/no-context/checkpoint-4956/pytorch_model.bin",
+    path_or_fileobj="./no-context/with-context/checkpoint-4956/pytorch_model.bin",
     path_in_repo="pytorch_model.bin",
     repo_id=repo_id,
 )
